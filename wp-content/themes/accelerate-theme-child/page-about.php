fdddc89836624a0f66314e1484b2479ed24a7bf7
ksnyder213 <thisiskatiemae@gmail.com>
2017-06-14 11:40:53 -0700

    Added custom fields to About page for images and services text

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -8,6 +8,26 @@
  */
 
 get_header(); ?>
+
+<?php 
+	$size = "full";
+	$image_1 = get_field('image_1'); 
+	$image_2 = get_field('image_2'); 
+	$image_3 = get_field('image_3'); 
+	$image_4 = get_field('image_4'); 
+	$service_title_1 = get_field('service_title_1');
+	$service_title_2 = get_field('service_title_2');
+	$service_title_3 = get_field('service_title_3');
+	$service_title_4 = get_field('service_title_4');
+	$service_description_1 = get_field('service_description_1');
+	$service_description_2 = get_field('service_description_2');
+	$service_description_3 = get_field('service_description_3');
+	$service_description_4 = get_field('service_description_4');
+	$contact_button = get_field('contact_button');
+?>
+
+
+
 <section class="about-page">
 	<div class="site-content">
 		<?php while ( have_posts() ) : the_post(); ?>
@@ -28,41 +48,41 @@ get_header(); ?>
 
 		<div class="service-block">
 			<figure class="service-icon left-service-icon">
-				<img src="http://localhost:8888/accelerate/wp-content/uploads/2017/06/bullseye-icon.jpg">
+				<?php echo wp_get_attachment_image( $image_1, $size ); ?>
 			</figure>
 			<div class="service-text right-service-text">
-				<h3>Content Strategy</h3>
-				<p>Bacon ipsum dolor sit amet strip steak jowl pancetta, cow turkey salami sausage fatback boudin biltong frankfurter shoulder pork turducken spare ribs. Ground round t-bone swine shoulder, cow bacon sausage turkey meatloaf. Tongue ball tip corned beef turkey. Leberkas shoulder pork chop doner venison meatball pig chicken spare.</p>
+				<h3><?php echo $service_title_1 ?></h3>
+				<p><?php echo $service_description_1 ?></p>
 			</div>
 		</div>
 
 		<div class="service-block">
 			<div class="service-text left-service-text">
-				<h3>Influencer Mapping</h3>
-				<p>Bacon ipsum dolor sit amet strip steak jowl pancetta, cow turkey salami sausage fatback boudin biltong frankfurter shoulder pork turducken spare ribs. Ground round t-bone swine shoulder, cow bacon sausage turkey meatloaf. Tongue ball tip corned beef turkey. Leberkas shoulder pork chop doner venison meatball pig chicken spare.</p>
+				<h3><?php echo $service_title_2 ?></h3>
+				<p><?php echo $service_description_2 ?></p>
 			</div>
 			<figure class="service-icon right-service-icon">
-				<img src="http://localhost:8888/accelerate/wp-content/uploads/2017/06/atom-icon.jpg">
+				<?php echo wp_get_attachment_image( $image_2, $size ); ?>
 			</figure>			
 		</div>
 
 		<div class="service-block">
 			<figure class="service-icon left-service-icon">
-				<img src="http://localhost:8888/accelerate/wp-content/uploads/2017/06/like-icon.jpg">
+				<?php echo wp_get_attachment_image( $image_3, $size ); ?>
 			</figure>
 			<div class="service-text right-service-text">
-				<h3>Social Media Strategy</h3>
-				<p>Bacon ipsum dolor sit amet strip steak jowl pancetta, cow turkey salami sausage fatback boudin biltong frankfurter shoulder pork turducken spare ribs. Ground round t-bone swine shoulder, cow bacon sausage turkey meatloaf. Tongue ball tip corned beef turkey. Leberkas shoulder pork chop doner venison meatball pig chicken spare.</p>
+				<h3><?php echo $service_title_3 ?></h3>
+				<p><?php echo $service_description_3 ?></p>
 			</div>			
 		</div>		
 
 		<div class="service-block">
 			<div class="service-text left-service-text">
-				<h3>Design and Development</h3>
-				<p>Bacon ipsum dolor sit amet strip steak jowl pancetta, cow turkey salami sausage fatback boudin biltong frankfurter shoulder pork turducken spare ribs. Ground round t-bone swine shoulder, cow bacon sausage turkey meatloaf. Tongue ball tip corned beef turkey. Leberkas shoulder pork chop doner venison meatball pig chicken spare.</p>
+				<h3><?php echo $service_title_4 ?></h3>
+				<p><?php echo $service_description_4 ?></p>
 			</div>
 			<figure class="service-icon right-service-icon">
-				<img src="http://localhost:8888/accelerate/wp-content/uploads/2017/06/magicwand-icon.jpg">
+				<?php echo wp_get_attachment_image( $image_4, $size ); ?>
 			</figure>
 		</div>
 	</div>
@@ -73,7 +93,7 @@ get_header(); ?>
 		<div class="about-contact-text">
 			<h4>Interested in working with us?</h4>
 			<a href="http://localhost:8888/accelerate/contact-us/" class="button contact-us-button">
-				Contact Us
+				<?php echo $contact_button ?>
 			</a>
 		</div>
 	</div>
